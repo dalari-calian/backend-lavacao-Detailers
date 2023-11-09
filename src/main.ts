4e4626693c4250775155379f232efb4082d37d37
calian-d <129885009+calian-d@users.noreply.github.com>
2023-11-09 14:44:51 -0300

    Adiciona pequenas alterações
    Adiciona pequenas alterações para conseguir fazer requisições do localhost

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const cors = require('cors');
+  const corsOptions = {
+    origin:'http://localhost:3000',
+    credentials:true,
+    optionSuccessStatus:200
+  }
   const app = await NestFactory.create(AppModule);
+  app.use(cors(corsOptions));
   await app.listen(3333);
 }
 bootstrap();
